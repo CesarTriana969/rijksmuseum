540026f2f0090d11632fc2c2d7f3c41644768396
Cesar Triana <catriana0398@gmail.com>
2024-05-30 20:23:10 -0500

    test

---
+++ pages/api/tokens.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { sql } from '@vercel/postgres';
+
+interface TokenRequestBody {
+  unq_id: string;
+}
+
+export default async function handlePostRequest(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Método no permitido' });
+  }
+
+  try {
+    if (req.headers['content-type'] !== 'application/json') {
+      return res.status(400).json({ error: 'Content-Type debe ser application/json' });
+    }
+
+    const { body } = req.body as { body: string };
+    let requestBody: TokenRequestBody;
+
+    try {
+      requestBody = JSON.parse(body);
+    } catch (error) {
+      return res.status(400).json({ error: 'El cuerpo de la solicitud no es un JSON válido.' });
+    }
+
+    if (!requestBody.unq_id) {
+      return res.status(400).json({ error: 'Campo unq_id requerido en el cuerpo de la solicitud.' });
+    }
+
+    await sql`
+      INSERT INTO tokens (unq_id)
+      VALUES (${requestBody.unq_id})
+    `;
+
+    return res.status(200).json({ message: 'Token almacenado exitosamente.' });
+  } catch (error) {
+    console.error('Error al manejar la solicitud POST:', error);
+    return res.status(500).json({ error: 'Error interno del servidor.' });
+  }
+}
